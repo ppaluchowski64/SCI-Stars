2809059ad3c0bbd7dc6698a77cc710b8a44445b0
Lelek <106480210+LelekDEV@users.noreply.github.com>
2024-12-26 19:34:44 +0100

    Add basic projectile explosion FX
    Also bring back accidentally deleted Pablo's README.md commit

--- game/scripts/projectile.gd
+++ game/scripts/projectile.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var Explosion = preload("res://scenes/explosion_fx.tscn")
+
 # Stats
 var speed: float = 50.0
 var distance: float = 7000.0
@@ -8,6 +10,15 @@ var damage: float = 750.0
 # Properties
 var travelled: float = 0
 var player_id: int
+
+func destroy() -> void:
+	var explosion = Explosion.instantiate()
+	
+	explosion.global_position = global_position
+	
+	get_parent().call_deferred("add_child", explosion)
+	
+	queue_free()
 
 func _process(delta: float) -> void:
 	velocity = Vector2(1, 0).rotated(rotation) * speed * delta * 1000
@@ -23,7 +34,7 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.id != player_id:
 			body.take_damage(damage)
-			queue_free()
+			destroy()
 			
 	elif body.is_in_group("obstacle"):
-		queue_free()
+		destroy()
